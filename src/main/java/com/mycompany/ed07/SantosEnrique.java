5e8422728f7f43ac3543e148565c15412bf74072
Enrique <Enrique@ASUS_BOOK>
2025-05-19 15:31:42 -0500

    Agregue mi nombre Kike

---
+++ src/main/java/com/mycompany/ed07/SantosEnrique.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ed07;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Enrique
+ */
+public class SantosEnrique {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Santos Morales Enrique :3");
+    }
+}
