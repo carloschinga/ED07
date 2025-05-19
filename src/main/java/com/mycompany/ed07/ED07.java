0d94ed47db7251558add112a027a8a0193898b0e
Usuario <Usuario@192.168.44.33>
2025-05-19 14:25:20 -0500

    agregue nombre

--- src/main/java/com/mycompany/ed07/ED07.java
+++ src/main/java/com/mycompany/ed07/ED07.java
@@ -12,5 +12,35 @@ public class ED07 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
+        
+        System.out.println("Jhoel Gavidia Calderon");   
     }
 }
