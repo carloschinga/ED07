32ed6014b363c56e8fe3d55accb68fdf331a5473
carloschinga <chinga_carlos@hotmail.com>
2025-05-19 14:01:09 -0500

    Proyecto inicial

---
+++ src/main/java/com/mycompany/ed07/ED07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ed07;
+
+/**
+ *
+ * @author USER
+ */
+public class ED07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
