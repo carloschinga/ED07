c6c97d88f500ca54bc43c3fd6cd52cd1d92e10da
Ariana <ariana.nikol30@gmail.com>
2025-05-19 15:27:49 -0500

    Agregue mi nombre Ariana

--- src/main/java/com/mycompany/ed07/ED07.java
+++ src/main/java/com/mycompany/ed07/ED07.java
@@ -13,7 +13,7 @@ public class ED07 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Santos Morales Enrique");
-        
+        System.out.println("Sanchez Barreto Ariana");
         
         
         
