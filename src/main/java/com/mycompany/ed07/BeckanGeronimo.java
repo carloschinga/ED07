de7ffebb53db9523005eea315e121df3d00ac306
chila <chila@chila5>
2025-05-19 20:48:51 -0500

    Merge origin/master
    Conflicts:
    	src/main/java/com/mycompany/ed07/ED07.java

---
+++ src/main/java/com/mycompany/ed07/BeckanGeronimo.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.ed07;
+
+/**
+ * @author Beckan Geronimo
+ */
+public class BeckanGeronimo {
+    public static void main(String[] args) {
+        System.out.println("Beckan Geronimo");
+    }
+}
