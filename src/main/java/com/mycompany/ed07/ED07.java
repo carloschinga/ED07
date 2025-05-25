c7a370094c5d12e2b437ff2f8d3037236af28e34
Fabian <0333232021@unjfsc.edu.pe>
2025-05-25 16:45:19 -0500

    iniciando

--- src/main/java/com/mycompany/ed07/ED07.java
+++ src/main/java/com/mycompany/ed07/ED07.java
@@ -14,33 +14,30 @@ import java.util.Stack;
 public class ED07 {
 
     public static void main(String[] args) {
-        Stack<Operadores> pilaOperadores= new Stack<Operadores>();
-        
         String formula="[({})]";
         String caracteresApertura="[({";
         
+        Stack<Character> pilaCaracteres = new Stack<>();
         
-        Operadores op1= new Operadores();
-        op1.setCodigo(1);
-        op1.setNombre("[");
-        
-        Operadores op2= new Operadores();
-        op2.setCodigo(2);
-        op2.setNombre("(");
-        
-        Operadores op3= new Operadores();
-        op3.setCodigo(3);
-        op3.setNombre("{");
-        
-        pilaOperadores.push(op1);
-        pilaOperadores.push(op2);
-        pilaOperadores.push(op3);
-        
-        System.out.println(pilaOperadores.peek().getNombre());
-        pilaOperadores.pop();
-        System.out.println(pilaOperadores.peek().getNombre());
-        pilaOperadores.pop();
-        System.out.println(pilaOperadores.peek().getNombre());
-        
+        for (int i = 0; i < formula.length(); i++) {
+            Character c = formula.charAt(i);
+            if (c.equals("[") || c.equals("(") || c.equals("{")) {
+                pilaCaracteres.push(c);
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            Character  complemento = 'a';
+            switch (pilaCaracteres.pop()) {
+                case '{': complemento = '}';
+                case '[': complemento = ']';
+                case '(': complemento = ')';
+                default: System.out.println("no hay");
+            }
+//            Character c = formula.charAt(i);
+//            if (c.equals(complemento)) {
+//                pilaCaracteres.pop();
+//            }
+            System.out.println(complemento);
+        }
     }
 }
---
+++ src/main/java/com/mycompany/ed07/MercadoArevaloFabian.java
@@ -0,0 +1,8 @@
+package com.mycompany.ed07;
+
+public class MercadoArevaloFabian {
+    
+    public static void main(String[] args) {
+        System.out.println("Fabian Mercado Arevalo");
+    }
+}
