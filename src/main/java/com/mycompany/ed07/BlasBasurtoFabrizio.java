b9046b22d43c0bf2bd82d1b6acb1d196afe8b9d3
USUARIO <USUARIO@DESKTOP-HG8AHLS>
2025-06-01 22:45:02 -0500

    Blas Basurto Fabrizio

---
+++ src/main/java/com/mycompany/ed07/BlasBasurtoFabrizio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ed07;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class BlasBasurtoFabrizio {
+    public static void main(String[] args) {
+        System.out.println("Blas Basurto Fabrizio");
+    }
+    
+}
